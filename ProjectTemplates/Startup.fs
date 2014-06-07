e23e11abcf11380842d1dd35796fd81ecb33dffb
Konstantin Tarkus <hello@tarkus.me>
2014-06-07 12:35:26 +0400

    Initial commit

---
+++ ProjectTemplates/Startup.fs
@@ -0,0 +1,14 @@
+﻿namespace $safeprojectname$
+
+open Owin
+open Microsoft.Owin
+
+type Startup() =
+    member x.Configuration(app: Owin.IAppBuilder) =
+        app.MapSignalR().Run(fun ctx ->
+            ctx.Response.ContentType <- "text/plain"
+            ctx.Response.WriteAsync("Web Application Server with F#, Owin, ASP.NET Identity, SignalR...")
+        ) |> ignore
+
+[<assembly: OwinStartup(typeof<Startup>)>]
+do ()
